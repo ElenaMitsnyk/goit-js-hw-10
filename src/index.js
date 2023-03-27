54f9d05ac1ba45e5464e4c4831fcb08208742e19
ElenaMitsnyk <elenamicnyk0410@gmail.com>
2023-03-28 01:35:17 +0200

    fetchCountries end

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
-import { fetchCountries } from './css/js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
